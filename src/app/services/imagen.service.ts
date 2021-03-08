a7c523b8474b3deeb4ddecd8201dbf2edec0b2da
PGO99 <pedrocala2@gmail.com>
2021-03-08 19:49:27 +0100

    subiendo

---
+++ src/app/services/imagen.service.ts
@@ -0,0 +1,41 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Imagen } from '../models/imagen';
+import { TokenService } from '../services/token.service'
+import { HttpHeaders } from '@angular/common/http'
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ImagenService {
+
+  imagenURL = 'http://localhost:8888/cloudinary/';
+/**
+ * @param httpClient
+ */
+  constructor(private httpClient: HttpClient, private tokenService: TokenService){ }
+/**
+ * @return this.httpClient.get<Imagen[]>(this.imagenURL + 'list')
+ */
+  public list(): Observable<Imagen[]> {
+    return this.httpClient.get<Imagen[]>(this.imagenURL + 'list',{ headers: new HttpHeaders().set('Authorization',`Bearer ${ this.tokenService.getToken()}`)});
+  }
+/**
+ * 
+ * @param imagen 
+ * @return this.httpClient.post<any>(this.imagenURL + 'upload', formData)
+ * 
+ */
+  public upload(imagen: File): Observable<any> {
+    const formData = new FormData();
+    formData.append('multipartFile', imagen);
+    return this.httpClient.post<any>(this.imagenURL + 'upload', formData,{ headers: new HttpHeaders().set('Authorization',`Bearer ${ this.tokenService.getToken()}`)});
+   }
+/**
+ * @return this.httpClient.delete<any>(this.imagenURL + `delete/${id}`)
+ */
+  public delete(id: number): Observable<any> {
+    return this.httpClient.delete<any>(this.imagenURL + `delete/${id}`,{ headers: new HttpHeaders().set('Authorization',`Bearer ${ this.tokenService.getToken()}`)});
+  }
+}
